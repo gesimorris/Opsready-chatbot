df7892fe093c595b11da23e1278a7d9523f0d1af
Blake Berge <blakeberge@Blakes-MacBook-Pro.local>
2025-11-20 18:54:01 -0800

    Blake and Gesi files merged

--- tools/tool_workspace_deficencies.py
+++ tools/tool_workspace_deficencies.py
@@ -40,14 +40,52 @@ async def get_workspace_deficiencies(workspace_name: str) -> List[TextContent]:
         if not workspace_id:
             return [TextContent(type="text", text="Failed to get workspace with that name")]
 
-        forms_url = (f"{BASE_URL}api/workspace/{workspace_id}/form?offset=0&limit=500&name=")
+        #forms_url = (f"{BASE_URL}api/workspace/{workspace_id}/form?offset=0&limit=500&name=")
 
-        forms_response = session.get(forms_url)
-        forms_response.raise_for_status()
-        forms= forms_response.json()
+        #forms_response = session.get(forms_url)
+        #forms_response.raise_for_status()
+        #forms= forms_response.json()
 
-        if not forms:
-            return [TextContent(type="text", text="Failed to get forms with that name")]
+        #if not forms:
+           # return [TextContent(type="text", text="Failed to get forms with that name")]
+        form_access_id = "7f08e35a-9f0b-45ff-a554-7caff337f664"  # defecincies form id
+        access_url = f"{BASE_URL}/api/form/access/{form_access_id}"
+        access_data = session.get(access_url).json()
+        template_id = access_data.get("template_id")
+
+        # Get form template
+        template_data = session.get(f"{BASE_URL}/api/form/template/{template_id}").json()
+        flex_id = template_data.get("flex_definition_id")
+
+        table = f"{BASE_URL}/api/table/{flex_id}"
+        table_response = session.get(table)
+        table_data = table_response.json()
+        table_url = f"{BASE_URL}/api/table/{flex_id}/query"
+
+        query_body = {
+            "filter": {
+                "$type": "id",
+                "operator": "equal",
+                "left": {"$type": "field", "field": "e00df19"},
+                "right": {"$type": "id", "value": workspace_id}  # workspace_id from workspace mapping
+            },
+            "limit": 100,
+            "offset": 0
+        }
+
+        results = session.post(table_url, json=query_body)
+
+        lines = []
+        for i, record in enumerate(results, start=1):
+            fields = record.get("fields", {})
+
+            deficiency = fields.get("7483694f-7f64-4dbc-9e10-a738ad9ad1e7", {}).get("value", "Unknown Deficiency")
+            status = fields.get("dc68de1b-8104-449b-8334-41c28e0e4c4d", {}).get("value", "Unknown Status")
+
+            lines.append(f"{i}. {deficiency} — {status}")
+
+        output = f"**Deficiencies in {workspace_name}:**\n" + "\n".join(lines)
+        print(output)
         
 
 
