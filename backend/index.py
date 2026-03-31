748380c4bb91265b24f76e572c879e5c1f384fb7
gesimorris <t00686038@mytru.ca>
2026-03-31 11:45:16 -0700

    cors fix

--- backend/index.py
+++ backend/index.py
@@ -19,8 +19,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:3000",
-        "http://localhost:5173",
-        "https://*.vercel.app",
+        "https://opsready-chatbot.vercel.app"
     ],
     allow_credentials=True,
     allow_methods=["*"],
